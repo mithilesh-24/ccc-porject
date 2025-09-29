a7fe095f939ba306ec3e14a1cb8e400b708a57d2
mithilesh-2406 <mithilesh24.2006@gmail.com>
2025-09-29 22:10:32 +0530

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Hero from './components/Hero';
 import './index.css';
 import ServiceCard from './components/ServiceCard';
+import './components/product';
+import Product from './components/product';
+import FeaturesSection from './components/FeaturesSection';
 function App() {
   return (
     <div className="App">
       <Hero />
       <ServiceCard />
+      <Product />
+      <FeaturesSection />
     </div>
   );
 }
