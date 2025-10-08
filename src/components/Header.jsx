38713978d37a91cb17b5b64415ad8eed9813cf59
mithilesh-2406 <mithilesh24.2006@gmail.com>
2025-10-08 22:19:29 +0530

    redo1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,11 +13,16 @@ function Header() {
                     <a href='#' className='nav'>Services</a>
                     <a href='#' className='nav'>Planters</a>
                     <a href='#' className='nav'>Contact</a>
-                    <a href='#' className='navbut'>Call</a>
+                    <button  className='navbut'><a href='#'>Call</a></button>
                 </div>
             </div>
             <div className='textcontent'>
-                <h1>NATURE BEAUTY</h1>
+                <h1>Nature's Beauty Delivered to You</h1>
+                <p>Nature's beauty is just a click away with our online flower and plant shop. We offer a<br/>wide variet of flower that will a touch of nature to your home!</p>
+                <div className='but'>
+                    <button className='but1'>Book now</button>
+                    <button className='but2'>Watch Video</button>
+                </div>
             </div>
         </div>
         </>
