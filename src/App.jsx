f2cd69ddf0487a60f33100e60c2f02aa8242e101
mithilesh-2406 <mithilesh24.2006@gmail.com>
2025-10-11 21:16:07 +0530

    research

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import About  from './components/Aboutus.jsx';
 import Feature from './components/Featurecard.jsx';
+import ResearchAndGallery from './components/ResearchAndGallery.jsx';
 import './index.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <About />
       <Feature />
+      <ResearchAndGallery />
     </div>
   );
 }
---
+++ src/components/ResearchAndGallery.jsx
@@ -0,0 +1,39 @@
+import '../styles/ResearchAndGallery.css';
+
+function ResearchAndGallery(){
+    return (
+        <>
+        <div className='research'>
+            <img src='/Feature_image.png'/>
+            <div className='research-content'>
+                <div className='research-card'>
+                    <img src='./Quality_Product_logo.png'/>
+                    <h3>Quality Product</h3>
+                    <p>
+                        our flower are of the highest quality. carefully selected and sourced from
+                    </p>
+                </div>
+                <div className='research-card' id='card'>
+                    <img src='./Grow_Sprout_logo.png'/>
+                    <h3>Always Fresh</h3>
+                    <p>Or flower are always fresh, handpicked and delivered promptly for maximum longevity and enjoyment.</p>
+                </div>
+                <div className='research-card' id='card'>
+                    <img src='./Temperature_logo.png'/>
+                    <h3>Work Smart</h3>
+                    <p>We work smart, using innovative techniques and technoloy to streamline and process</p>
+                </div>
+                <div className='research-card' >
+                    <img src='./Pruning_logo.png'/>
+                    <h3>Excelent Service</h3>
+                    <p>We pride ourselves on providing excellent services, going above and beyond to meet our customer's needs</p>
+                </div>
+            </div>
+        </div>
+        <div className='gallery'>
+            
+        </div>
+        </>
+    );
+}
+export default ResearchAndGallery;
