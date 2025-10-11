bb523f3b1fedadb51fd173f4b228a388e877864e
mithilesh-2406 <mithilesh24.2006@gmail.com>
2025-10-11 16:12:42 +0530

    feature card

--- src/components/Featurecard.jsx
+++ src/components/Featurecard.jsx
@@ -4,7 +4,92 @@ import '../styles/Featurecard.css';
 function feature(){
     return (
         <>
-        
+        <div className='featurecard'>
+            <h1>What We Offer To You</h1>
+            <div className='cardbox'>
+                <div className='card'>
+                    <img src='./Product_Plant_1.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_2.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_3.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_4.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_5.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_6.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_7.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>($10)</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+                <div className='card'>
+                    <img src='./Product_Plant_8.png'/>
+                    <div className='card-content'>
+                        <div>
+                            <h3>Cactus Plant</h3>
+                            <p><strick>$10</strick> $8</p>
+                        </div>
+                        <a href='#'>Buy Now</a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
         </>
     );
 }
